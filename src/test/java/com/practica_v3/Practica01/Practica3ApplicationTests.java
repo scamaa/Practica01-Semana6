97c1f7ae66698aa2fe8cf62b2fcd0635343adc26
Santiago <Santiago@SantiPC.tigo.co.cr>
2023-03-02 16:27:24 -0600

    Practica 01 Semana 6 / Version 2

--- src/test/java/com/practica_v3/Practica01/Practica3ApplicationTests.java
+++ src/test/java/com/practica_v3/Practica01/Practica3ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Practica01ApplicationTests {
+class Practica3ApplicationTests {
 
 	@Test
 	void contextLoads() {
